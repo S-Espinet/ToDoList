9302d46ab9c53e48fc02adffd1d5e2e366687cc7
S. Espinet <s.lou.espinet@gmail.com>
2022-02-28 17:11:37 -0800

    add ASP.NET MVC Core functionality

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,4 +1,45 @@
-using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ToDoList
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*using System;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,5 +86,4 @@ namespace ToDoList
       }
     }
   }
-}
-
+}*/
