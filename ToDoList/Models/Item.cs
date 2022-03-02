a5233642c05e7e4dc5298c5a0f9c2e93b8ae2443
S. Espinet <s.lou.espinet@gmail.com>
2022-03-01 18:15:51 -0800

    add search by ID method, test

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -19,6 +19,11 @@ namespace ToDoList.Models
     {
       _instances.Clear();
     }
+
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
     
     //Constructor
     public Item (string description)
