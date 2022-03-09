ad70ae569455db3008be0045b11a27875de7eb93
S. Espinet <s.lou.espinet@gmail.com>
2022-03-09 11:58:06 -0800

    add delete and edit functionality with Entity
    Co-authored-by: marisa-edgar <marisa-edgar@users.noreply.github.com>
    Co-authored-by: Hayley McVay <hmcvay@users.noreply.github.com>

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -1,69 +1,75 @@
-// using System.Collections.Generic;
-// using System;
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoList.Models;
 
-// namespace ToDoList.Controllers
-// {
-//   public class CategoriesController : Controller
-//   {
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-//     [HttpGet("/categories")]
-//     public ActionResult Index()
-//     {
-//       List<Category> allCategories = Category.GetAll();
-//       return View(allCategories);
-//     }
+    public CategoriesController(ToDoListContext db)
+    {
+      _db = db;
+    }
 
-//     [HttpGet("/categories/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
+    public ActionResult Index()
+    {
+      List<Category> model = _db.Categories.ToList();
+      ViewBag.PageTitle = "View All Categories";
+      return View(model);
+    }
 
-//     [HttpPost("/categories")]
-//     public ActionResult Create(string categoryName)
-//     //create lets us process submissions from the form located in New; this post request makes the actual changes the user requested in that form, so the paths are the same for these two routes
-//     {
-//       Category newCategory = new Category(categoryName);
-//       //this route accepts a categoryName argument. This refers to the contents of the category name form field in the New view.
-//       return RedirectToAction("Index");
-//       //Index send the user back to the index route (at the top of this list)
-//     }
+    public ActionResult Create()
+    {
+      return View();
+    }
 
-//     [HttpGet("/categories/{id}")]
-//     //this is the page that will show - the id's page, which shows a category's details (the category's details are the tasks, i.e. Items, that are in that category) - it knows which category to show from the dynamically-generated id
-//     public ActionResult Show(int id)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category selectedCategory = Category.Find(id);
-//       List<Item> categoryItems = selectedCategory.Items;
-//       model.Add("category", selectedCategory);
-//       model.Add("items", categoryItems);
-//       //the two lines above create a dictionary called model, which holds key value pairs. The keys are the category and item strings, which are ideas (or classes?) and the values are the selected category (category object), and its associated items. We can pass the whole dictionary into view.
-//       return View(model);
-//     }
+    [HttpPost]
+    public ActionResult Create(Category category)
+    {
+      _db.Categories.Add(category);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-//     [HttpPost("categories/{categoryId}/items")]
-//     //this did go in items controller, but the act of adding an item changes the category object, so we include it with other category object routes
-//     public ActionResult Create(int categoryId, string itemDescription)
-//     //create needs the category id now, as well as the description of the item because items live inside categories
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category foundCategory = Category.Find(categoryId);
-//       //using the id from above, we have to find the category object that we're adding the item to
-//       Item newItem = new Item(itemDescription);
-//       //the line above is from the user's input in the form
-//       newItem.Save();
-//       //this line saves to database
-//       foundCategory.AddItem(newItem);
-//       List<Item> categoryItems = foundCategory.Items;
-//       //the view requires all of the items in this list because when it renders, it displays all of the items in the list that belongs to this category
-//       model.Add("items", categoryItems);
-//       model.Add("category", foundCategory);
-//       return View("Show", model);
-//     }
-//   }
-// }
+    public ActionResult Details(int id)
+    {
+      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+
+    [HttpPost]
+    public ActionResult Edit (Category category)
+    {
+      _db.Entry(category).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      _db.Categories.Remove(thisCategory);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+ }
+}
 
 //paths for all item routes will now include a fragment from the parent category before the item information
